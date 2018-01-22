22a0faf13722b7fb1ba4ed27c1f1c0b77a1ff62a
Alexandre Resende <alexandre.amres@gmail.com>
2018-01-22 21:26:31 -0200

    feature: Imṕlement user controller

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,10 +1,50 @@
 
 const Joi = require('joi');
 
-module.exports.getUser = () => {};
+module.exports.getUser = (req, res, next) => {
+  //call model
+};
 
-module.exports.addUser = () => {};
+module.exports.addUser = (req, res, next) => {
+  const userSchema = {
+    name: Joi.string().required(),
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+    email: Joi.string().email().required(),
+    date_of_birth: Joi.string().required()
+  };
+  const userValidation = Joi.validate(req.body, userSchema);
 
-module.exports.editUser = () => {};
+  Promise
+    .all([userValidation])
+    .then(result => {
+      //call model
+    })
+    .catch(() => {
+      res.status(500).send({ error: 'An error occurred in addUser controller' });
+    });
+};
 
-module.exports.deleteUser = () => {};
+module.exports.editUser = (req, res, next) => {
+  const userSchema = {
+    name: Joi.string().optional(),
+    username: Joi.string().optional(),
+    password: Joi.string().optional(),
+    email: Joi.string().email().optional(),
+    date_of_birth: Joi.string().optional()
+  };
+  const userValidation = Joi.validate(req.body, userSchema);
+
+  Promise
+    .all([userValidation])
+    .then(result => {
+      //call model
+    })
+    .catch(() => {
+      res.status(500).send({ error: 'An error occurred in editUser controller' });
+    });
+};
+
+module.exports.deleteUser = (req, res, next) => {
+  //call model
+};
